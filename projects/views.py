bbee957c50d38151ff07233ec8886e0fdbc19f01
shelen ells <shelen.ells@gmail.com>
2022-05-09 19:19:22 -0700

    feature8

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from projects.models import Project
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here
 # 1st- list view
@@ -9,10 +10,15 @@ from projects.models import Project
 #     template_name = "model_names/list.html"
 
 
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/list.html"
 
+    def form_valid(self, form):
+        item = form.save(commit=False)
+        item.owner = self.request.user
+        item.save()
+        return redirect("home")
 
-    # def get_queryset(self):
-    #     return Project.objects.filter(members=self.request.user)
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
