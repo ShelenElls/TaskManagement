c3cee996332b6b5325d305f2b27854ef277cdc22
shelen ells <shelen.ells@gmail.com>
2022-05-09 17:26:58 -0700

    Feature5

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import redirect
+from django.views.generic.list import ListView
+from projects.models import Project
 
-# Create your views here.
+# Create your views here
+# 1st- list view
+# class ModelNameListView(ListView):
+#     model = ModelName
+#     template_name = "model_names/list.html"
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = "projects/list.html"
+
+    # def get_queryset(self):
+    #     return Project.objects.filter(members=self.request.user)
