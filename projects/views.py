93c12690f03ff1689420e3307600797e11c4b201
shelen ells <shelen.ells@gmail.com>
2022-05-10 08:51:29 -0700

    feature14

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from projects.models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here
 # 1st- list view
@@ -31,3 +33,12 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
 
     def get_queryset(self, **kwargs):
         return Project.objects.filter(members=self.request.user)
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "projects/create.html"
+    fields = ["name", "description", "members"]
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("show_project", args=[self.object.id])
