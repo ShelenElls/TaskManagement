a1ddf808a4aec3dcff8ec951b7efea5f58a427b5
shelen ells <shelen.ells@gmail.com>
2022-05-09 19:42:57 -0700

    feature10

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        banana = UserCreationForm(request.POST)
+        if banana.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+            )
+            user.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        banana = UserCreationForm()
+    context = {
+        "form": banana,
+    }
+    return render(request, "registration/signups.html", context)
