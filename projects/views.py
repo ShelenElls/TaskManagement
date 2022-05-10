aa0063007ffa7485668f2883f52c55c9384e4b53
shelen ells <shelen.ells@gmail.com>
2022-05-09 23:49:06 -0700

    eve

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from projects.models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -21,4 +22,12 @@ class ProjectListView(LoginRequiredMixin, ListView):
         return redirect("home")
 
     def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
+    template_name = "projects/detail.html"
+
+    def get_queryset(self, **kwargs):
         return Project.objects.filter(members=self.request.user)
